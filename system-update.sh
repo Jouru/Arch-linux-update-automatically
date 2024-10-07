90e12f0c425c4efb87151b6b2ab3cd485dfc7df6
Jouru <104511948+Jouru@users.noreply.github.com>
2024-10-07 13:04:47 +0200

    Update system-update.sh

--- system-update.sh
+++ system-update.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/zsh
 
 updated="'System Updated' 'Archlinux has been updated successfully. Dont forget update AUR packeges'"
-failed="'Update failed' 'Something goes wrong pacman cannot update the system. Use systemctl status system_update.service for more information'"
+failed="'Update failed' 'Something went wrong pacman cannot update the system. Use systemctl status system_update.service for more information'"
 
 pacman -Syu --noconfirm &&\
 sudo -u user DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send $updated||\
